e6f701fef76a9009956a4a22ca2a5bcf1fcdbd70
kf5131 <kf5131@icloud.com>
2024-01-30 15:50:15 +0100

    add canvas

--- src/app/view.py
+++ src/app/view.py
@@ -5,8 +5,11 @@ from typing import Protocol
     # title
 TITLE = "Pathfinding Visualizer"
     # window size
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 800
+    # canvas size
+CANVAS_WIDTH = WINDOW_WIDTH - 10
+CANVAS_HEIGHT = WINDOW_HEIGHT - 10
 
 class Presenter(Protocol):
     def update_view(self, event=None) -> None:
@@ -22,6 +25,9 @@ class View(tk.Tk):
     def init_ui(self, presenter: Presenter) -> None:
         self.frame = tk.Frame(self, padx=10, pady=10)
         self.frame.pack(fill=tk.BOTH, expand=True)
+        
+        self.canvas = tk.Canvas(self.frame, bg='white',width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
+        self.canvas.pack()
         
     def update_view(self, event=None) -> None:
         ...
