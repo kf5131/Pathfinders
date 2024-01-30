cf365d87dcc6f530738be712f80ce2d85a5acc85
kf5131 <kf5131@icloud.com>
2024-01-30 15:50:05 +0100

    add algo list

--- src/app/model.py
+++ src/app/model.py
@@ -2,7 +2,8 @@
 
 class Model:
     def __init__(self):
-        self.data = "Hello World!"
+        self.data = "Hello World!" # debug data
+        self.algos = ["Dijkstra", "A*", "BFS", "DFS"] # list of algorithms
         
         
     def get_data(self):
